9d5b535e038b6506f94d389930f68d25519ec8ff
Jhon <jzentenos@unjbg.edu.pe>
2026-09-23 10:03:06 -0500

    Analisis de rendimiento y medición de tiempo con 1, 5 y 10 hilos

--- hilos.c
+++ hilos.c
@@ -1,34 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <time.h>
 
-// Declaracion del mutex global
-pthread_mutex_t mutex;
+pthread_mutex_t mutex; // Declaracion del mutex
 
-void* hilo_funcion_con_mutex(void* arg) {
+// Funcion del hilo
+void* hilo_funcion(void* arg) {
     pthread_mutex_lock(&mutex); // Bloquear el mutex
     printf("Hola desde el hilo %ld\n", (long)arg);
     pthread_mutex_unlock(&mutex); // Desbloquear el mutex
     pthread_exit(NULL);
 }
 
-int main() {
-    pthread_t hilo1, hilo2;
-
+void ejecutar_con_hilos(int num_hilos) {
+    pthread_t hilos[num_hilos];
     // Inicializar el mutex
     pthread_mutex_init(&mutex, NULL);
-
-    // Creacion de los hilos utilizando hilo_funcion_con_mutex
-    pthread_create(&hilo1, NULL, hilo_funcion_con_mutex, (void*)1);
-    pthread_create(&hilo2, NULL, hilo_funcion_con_mutex, (void*)2);
-
+    // Medicion de tiempo
+    clock_t start, end;
+    start = clock(); // Inicio del temporizador
+    // Crear hilos
+    for (long i = 0; i < num_hilos; i++) {
+        pthread_create(&hilos[i], NULL, hilo_funcion, (void*)i);
+    }
     // Esperar a que los hilos terminen
-    pthread_join(hilo1, NULL);
-    pthread_join(hilo2, NULL);
-
-    // Destruir el mutex al final del programa
+    for (int i = 0; i < num_hilos; i++) {
+        pthread_join(hilos[i], NULL);
+    }
+    end = clock(); // Fin del temporizador
+    // Destruir el mutex
     pthread_mutex_destroy(&mutex);
+    // Calcular y mostrar el tiempo total de ejecucion
+    double cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("Tiempo total de ejecucion con %d hilos: %f segundos\n\n", num_hilos, cpu_time_used);
+}
 
-    printf("Finalizo la ejecucion del programa principal\n");     
+int main() {
+    printf("Ejecutando con 1 hilo...\n");
+    ejecutar_con_hilos(1);
+    printf("Ejecutando con 5 hilos...\n");
+    ejecutar_con_hilos(5);
+    printf("Ejecutando con 10 hilos...\n");
+    ejecutar_con_hilos(10);
+
+    printf("Finalizo la ejecucion del programa principal\n");
     return 0;
 }
